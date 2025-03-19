6db750fb67cab5a2bc25db4aa789131e62feef2a
kunleoduntan <kunleoduntan@gmail.com>
2025-03-19 16:44:54 +0100

    n1
    n1

---
+++ general/general_biz_apps/doctype/supplier_opportunity/test_supplier_opportunity.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, kunleadenuga and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSupplierOpportunity(FrappeTestCase):
+	pass
