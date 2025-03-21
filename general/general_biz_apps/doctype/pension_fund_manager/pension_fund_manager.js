29a1e36490a9bf52f8af73a1721c0f5d9646c2c6
kunleoduntan <kunleoduntan@gmail.com>
2025-03-21 16:55:11 +0100

    h2

--- general/general_biz_apps/doctype/pension_fund_manager/pension_fund_manager.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, kunleadenuga and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Pension Fund Manager', {
-	// refresh: function(frm) {
-
-	// }
-});
--- general/general_biz_apps/doctype/waybill/waybill.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, kunleadenuga and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Waybill", {
-// 	refresh(frm) {
-
-// 	},
-// });
