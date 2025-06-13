30639a4e01973085dce408da8df3318865ba2260
Kunleoduntan <kunleoduntan@gmail.com>
2025-06-13 15:39:08 +0100

    commits

---
+++ general/general_biz_apps/doctype/customer_transaction_history/customer_transaction_history.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, kunleadenuga and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CustomerTransactionHistory(Document):
+	pass
--- general/general_biz_apps/doctype/payment_and_purchase_request/payment_and_purchase_request.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, kunleadenuga and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Payment and Purchase Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
